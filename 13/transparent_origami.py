9769ed0c19e00d0189507d554d1c7ef76ed2c3a7
LucasJSilva <lucasjsilva_0xf0@protonmail.com>
2021-12-13 15:15:24 -0300

    second star day 13

--- 13/transparent_origami.py
+++ 13/transparent_origami.py
@@ -1,58 +1,43 @@
-def count_dots(paper):
-    count = 0
-    
-    for y in paper:
-        for x in y:
-            if x == "#":
-                count += 1
-    return count
-
-def fold_x(paper, pos):
-    for y in range(len(paper)):
-        for x in range(pos):
-            if paper[y][x] == "#":
-                paper[y][x*-1-1] = "#"
-                paper[y][x] = " "
-
-def fold_y(paper, pos):
-    for x in range(len(paper[0])):
-        for y in range(pos):
-            if paper[y*-1-1][x] == "#":
-                paper[y][x] = "#"
-                paper[y*-1-1][x] = " "
-            
-
 def main(filename):
     with open(filename) as file:
         data = [ line.replace("\n", "").split(",") for line in file.readlines() ]
         coordinates = [(int(x), int(y)) for x, y in data[:data.index([""])]]
         folds = [ data[0].replace("fold along ", "") for data in data[data.index([""])+1:]]
+        paper = {}
 
-        xsize = 0
-        ysize = 0
         for x, y in coordinates:
-            if xsize < x:
-                xsize = x
+            paper[(x, y)] = True
 
-            if ysize < y:
-                ysize = y
-        
-        paper = [[" " for _ in range(xsize+1)] for _ in range(ysize+1)]
-        
-        for x, y in coordinates:
-            paper[y][x] = "#"
+        for fold in folds:
+            type, pos = fold.split("=")
+            pos = int(pos)
+            p = {}
 
-        type, pos = folds[0].split("=")
+            if type == 'x': 
+                for (x, y) in paper:
+                    if x < pos:
+                        p[(x, y)] = True
+                    else:
+                        p[(pos-(x-pos), y)] = True
 
-        if type == "y":
-            fold_y(paper, int(pos))
-        else:
-            fold_x(paper, int(pos))
+            if type == "y":
+                for (x, y) in paper:
+                    if y < pos:
+                        p[(x, y)] = True
+                    else:
+                        p[(x, pos-(y-pos))] = True
 
-        print("dots: ", count_dots(paper))
+            paper = p
 
+        x = max([ x for x, _ in paper.keys() ])
+        y = max([ y for _, y in paper.keys() ])
 
-
+        buff = ''
+        for y in range(y+1):
+            for x in range(x+1):
+                buff += "#" if (x, y) in paper else ' '
+            print(buff)
+            buff = ''
 
 from sys import argv
 if __name__ == "__main__":
