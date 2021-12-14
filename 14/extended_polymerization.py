7c550614d1d9351fcf66e51a653c59640c97ad66
LucasJSilva <lucasjsilva_0xf0@protonmail.com>
2021-12-14 11:47:37 -0300

    second star day 14

--- 14/extended_polymerization.py
+++ 14/extended_polymerization.py
@@ -1,29 +1,34 @@
-def insert(polymer, obj):
-    return polymer[0] + obj + polymer[1]
+from collections import Counter
 
 def main(filename):
     with open(filename) as file:
         template = file.readline().replace("\n", "")
-        rules = {}
 
+        rules = {}
         for line in file.readlines():
             if line != "\n":
                 line = line.replace("\n", "").split(" -> ")
                 rules[line[0]] = line[1]
 
-        pool = [ char for char in template ]
+        counter = Counter()
+        for i in range(len(template)-1):
+            counter[template[i]+template[i+1]] += 1
 
-        final = 10
-        step = 0
-        while(step < final):
-            npool = " "
-            for polymer in [ pool[i] + pool[i+1] for i in range(len(pool)-1)]:
-                npool = npool[0:-1] + insert(polymer, rules[polymer])
-            pool = npool
-            step += 1
+        for i in range(40):
+            counter2 = Counter()
+            for k in counter:
+                counter2[k[0]+rules[k]] += counter[k]
+                counter2[rules[k]+k[1]] += counter[k]
+            counter = counter2
 
-        counts = [ pool.count(char) for char in set(pool) ]
-        print("Result: ", max(counts) - min(counts))
+        counter_chars = Counter()
+        for k in counter:
+            counter_chars[k[0]] += counter[k]
+        counter_chars[template[-1]] += 1
+            
+        print(counter_chars)
+
+        print("Result: ", max(counter_chars.values()) - min(counter_chars.values()))
             
 from sys import argv
 if __name__ == "__main__":
